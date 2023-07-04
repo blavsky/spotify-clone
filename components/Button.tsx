e6d73b7f9e742505c551da2a32f48a5f71add752
blavsky <vhostok@gmail.com>
2023-07-04 14:11:15 +0200

    Adding authentication modal and functionality

--- components/Button.tsx
+++ components/Button.tsx
@@ -13,7 +13,21 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
   return(
     <button 
       type={type}
-      className={twMerge(`w-full rounded-full bg-green-500 border border-transparent px-6 py-2 disabled:cursor-not-allowed disabled:opacity-50 text-black font-bold hover:opacity-75 transition`, className)}
+      className={twMerge(`
+        w-full 
+        rounded-full 
+        bg-green-500 
+        border 
+        border-transparent 
+        px-6 
+        py-2 
+        disabled:cursor-not-allowed 
+        disabled:opacity-50 
+        text-black 
+        font-bold 
+        hover:opacity-75 
+        transition
+      `, className)}
       disabled={disabled}
       ref={ref}
       {...props}
--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -25,9 +25,27 @@ const ListItem:React.FC<ListItemProps> = ({
   return ( 
     <button 
       onClick={onClick}
-      className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4"
+      className="
+        relative 
+        group 
+        flex 
+        items-center 
+        rounded-md 
+        overflow-hidden 
+        gap-x-4 
+        bg-neutral-100/10 
+        hover:bg-neutral-100/20 
+        transition 
+        pr-4
+      "
     >
-      <div className="relative min-h-[64px] min-w-[64px]">
+      <div 
+        className="
+          relative 
+          min-h-[64px] 
+          min-w-[64px]
+        "
+      >
         <Image 
           className="object-cover"
           fill
@@ -35,11 +53,30 @@ const ListItem:React.FC<ListItemProps> = ({
           alt="Image"
         />
       </div>
-      <p className="font-medium truncate py-5">
+      <p 
+        className="
+          font-medium 
+          truncate py-5
+        "
+      >
         {name}
       </p>
       <div 
-        className="absolute transition opacity-0 rounded-full flex items-center justify-center bg-green-500 p-4 drop=shadow-md right-5 group-hover:opacity-100 hover:scale-110"
+        className="
+          absolute 
+          transition 
+          opacity-0 
+          rounded-full 
+          flex 
+          items-center 
+          justify-center 
+          bg-green-500 
+          p-4 
+          drop=shadow-md 
+          right-5 
+          group-hover:opacity-100 
+          hover:scale-110
+        "
       >
         <FaPlay className="text-black"/>
       </div>
